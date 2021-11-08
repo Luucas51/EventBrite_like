c0bb961cfeb1c68f7904a8217a94c65f7aec573e
Luucas51 <oudartlucas51@gmail.com>
2021-11-08 14:38:16 +0100

    Mailer for attendance debug

--- app/mailers/attendance_mailer.rb
+++ app/mailers/attendance_mailer.rb
@@ -1,14 +1,14 @@
 class AttendanceMailer < ApplicationMailer
     default from: 'oudartlucas51@gmail.com'
  
-    def attendance_email(user)
+    def attendance_email(attendance)
       #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
-      @user = user 
+      @attendance = attendance 
   
       #on définit une variable @url qu'on utilisera dans la view d’e-mail
       @url  = 'http://monsite.fr/login' 
   
       # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-      mail(to: @user.email, subject: 'Ton nouveau event !') 
+      mail(to: @attendance.user.email, subject: 'Ton nouveau event !') 
     end
 end
