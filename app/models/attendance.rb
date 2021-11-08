ccea57cc0e28ad39f42bd1e06ac1cc0fcae6d1c0
Luucas51 <oudartlucas51@gmail.com>
2021-11-08 13:51:03 +0100

    Add attendance mail confirmation

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,10 @@
 class Attendance < ApplicationRecord
+    after_create :attendace_send
+
+    def attendace_send
+      AttendanceMailer.attendance_email(self).deliver_now
+    end
+
     belongs_to :user
     belongs_to :event
 end
