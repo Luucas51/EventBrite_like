18f91ec71d4ff2c86b0e16ba4e8eed254f042cb8
Luucas51 <oudartlucas51@gmail.com>
2021-11-09 13:20:11 +0100

    View login/logout... display if connected/no

--- app/mailers/attendance_mailer.rb
+++ app/mailers/attendance_mailer.rb
@@ -6,7 +6,7 @@ class AttendanceMailer < ApplicationMailer
       @attendance = attendance 
   
       #on définit une variable @url qu'on utilisera dans la view d’e-mail
-      @url  = 'http://monsite.fr/login' 
+      @url  = 'https://eventbrite-lucas.herokuapp.com/' 
   
       # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
       mail(to: @attendance.user.email, subject: 'Ton nouveau event !') 
