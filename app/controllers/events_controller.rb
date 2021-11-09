23859db6f86ed0e6c6cb356ace5c220f805c6384
Luucas51 <oudartlucas51@gmail.com>
2021-11-09 17:30:02 +0100

    Add event view ect

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,43 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
   end
   def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(
+      title: event_params[:title],
+      description: event_params[:description],
+      start_date: event_params[:start_date],
+      duration: event_params[:duration],
+      price: event_params[:price],
+      location: event_params[:location],
+      user: current_user
+    )
+    if @event.save
+      flash[:event_create] = "Event bien crée."
+      redirect_to event_path(@event.id)
+    else
+      messages = []
+      if @event.errors.any? 
+        @event.errors.full_messages.each do |message| 
+          messages << message
+        end 
+        flash[:event_error] = "Voici les erreurs : #{messages.join(" ")}"
+        render 'new'
+      end
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:events).permit(:title, :description, :start_date, :duration, :price, :location, :user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'events#index'
-  resources :events
+  resources :events, only: [:show, :create, :index, :new]
   devise_for :users
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
