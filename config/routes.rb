da0aeb9347ca3e4f3857dea3127cbe4cd04be50f
Luucas51 <oudartlucas51@gmail.com>
2021-11-11 10:50:58 +0100

    Add avatar

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   root to: 'events#index'
   resources :events, only: [:show, :create, :index, :new]
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :avatars, only: [:create]
+  end
   resources :events do
     resources :charges
     resources :attendances
   end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
