6b2f461a5fe55a3f4d5e27c2bb0cbd0aa288dd25
Luucas51 <oudartlucas51@gmail.com>
2021-11-12 12:34:26 +0100

    Add function edit/delete event at dashboard admin

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,2 +1,33 @@
 class Admin::EventsController < ApplicationController
+    def index
+
+    end
+    
+    def show 
+        @event = Event.find(params[:id])
+    end
+
+    def update
+        @event = Event.find(params[:id])
+ 
+        if @event.update('title' => params[:title],
+                        'description' => params[:description],
+                        'price' => params[:price])
+        redirect_to admin_event_path(@event.id)
+        else
+          render :edit
+        end
+    
+    end
+
+    def edit
+        @event = Event.find(params[:id])
+    end
+
+
+    def destroy
+        Event.find(params[:id]).destroy
+        redirect_to admin_index_path
+    end
+
 end
